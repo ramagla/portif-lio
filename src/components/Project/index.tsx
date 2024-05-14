3580d65546dd32bf15cffe415ce9235391dc2cb7
Rafael Almeida <dealmeida.silva@hotmail.com>
2024-05-14 10:41:57 -0300

    criado um card com imagens

--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -1,44 +1,75 @@
-import { Card, ButtonLink, ButtonLink2 } from './styles'
+import { Card, ButtonLink, ButtonLink2, ImageWrapper } from './styles'
 import Paragraph from '../Paragraph'
 import Title from '../Title'
+import efood from '../../assets/images/efood.png'
+
+//Projetos github
 
 export const Project1 = () => (
   <Card>
-    <Title>Projeto Contador de eventos</Title>
+    <ImageWrapper>
+      <img src={efood} alt="" />
+    </ImageWrapper>
+    <Title>Projeto site de Delivery</Title>
     <Paragraph type="secondary">
-      Contador de eventos feito com HTML, SCSS e JS
+      Para este projeto, optei por utilizar React juntamente com TypeScript. A
+      escolha do React permitiu-me criar uma interface de usuário interativa e
+      eficiente, enquanto o TypeScript ofereceu um sistema de tipos robusto que
+      melhorou a qualidade do código e facilitou a manutenção.
     </Paragraph>
     <ButtonLink
-      href="https://github.com/ramagla/contador_eventos"
+      title="clique aqui para ir para o projeto no github"
+      href="https://github.com/ramagla/eFood"
       target="blank"
     >
       Github
     </ButtonLink>
-    <ButtonLink2 href="https://contador-eventos.vercel.app/" target="blank">
+    <ButtonLink2
+      title="clique aqui para ir para o projeto na vercel"
+      href="https://e-food-inky.vercel.app/"
+      target="blank"
+    >
       Vercel
     </ButtonLink2>
   </Card>
 )
 export const Project2 = () => (
   <Card>
-    <Title>Projeto Contador de eventos2</Title>
+    <ImageWrapper>
+      <img src={efood} alt="" />
+    </ImageWrapper>
+    <Title>Projeto site de Games</Title>
     <Paragraph type="secondary">
-      Contador de eventos feito com HTML, SCSS e JS
+      Neste projeto, desenvolvi uma loja virtual de games, implementada com o
+      uso de React e TypeScript. A combinação dessas tecnologias permitiu a
+      criação de uma plataforma robusta e escalável, proporcionando uma
+      experiência de usuário fluida e dinâmica, além de garantir um código mais
+      seguro e fácil de manter.
     </Paragraph>
     <ButtonLink
-      href="https://github.com/ramagla/contador_eventos"
+      title="clique aqui para ir para o projeto no github"
+      href="https://github.com/ramagla/eplay"
       target="blank"
     >
       Github
     </ButtonLink>
-    <ButtonLink2 href="https://contador-eventos.vercel.app/" target="blank">
+    <ButtonLink2
+      title="clique aqui para ir para o projeto na vercel"
+      href="https://eplay-khaki-nine.vercel.app/"
+      target="blank"
+    >
       Vercel
     </ButtonLink2>
   </Card>
 )
 
+//Projetos powerbi
+
 export const Project3 = () => (
   <Card>
+    <ImageWrapper>
+      <img src={efood} alt="" />
+    </ImageWrapper>
     <Title>Projeto teste</Title>
     <Paragraph type="secondary">
       Contador de eventos feito com HTML, SCSS e JS
--- src/components/Project/styles.ts
+++ src/components/Project/styles.ts
@@ -3,16 +3,22 @@ import styled from 'styled-components'
 export const Card = styled.div`
   border: 1px solid ${(props) => props.theme.edgeColor};
   padding: 16px;
+  border-radius: 10px; /* Adicione bordas arredondadas */
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); /* Adicione sombra */
+  background-color: #f0f0f0; /* Defina uma cor de fundo */
+  margin: 10px; /* Ajuste as margens conforme necessário */
 `
+
 export const ButtonLink = styled.a`
-  color: ${(props) => props.theme.backgroundColor};
+  color: white;
   font-size: 14px;
-  background-color: ${(props) => props.theme.buttonBackgroundColor};
+  background-color: #007bff;
   text-decoration: none;
-  padding: 8px;
+  padding: 10px 20px;
+  border-radius: 5px;
   display: inline-block;
-  margin-top: 24px;
   cursor: pointer;
+  margin-top: 24px;
 `
 export const ButtonLink2 = styled.a`
   color: ${(props) => props.theme.backgroundColor};
@@ -24,4 +30,15 @@ export const ButtonLink2 = styled.a`
   display: inline-block;
   margin-top: 24px;
   cursor: pointer;
+`
+export const ImageWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: auto;
+  margin-bottom: 16px;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
 `
